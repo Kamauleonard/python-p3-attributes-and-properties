85e182cbb862dd3eb27848ee1919b671a81c8e57
Kamauleonard <leonardmwangi2012@gmail.com>
2023-12-14 14:11:30 +0300

    Completed Assignments

--- lib/dog.py
+++ lib/dog.py
@@ -1,34 +1,54 @@
-class Dog:
-    approved_breeds = ["Mastiff", "Chihuahua", "Corgi", "Shar Pei", "Beagle", "French Bulldog", "Pug", "Pointer"]
+#!/usr/bin/env python3
 
-    def __init__(self, name="", breed=""):
-        self._name = None
-        self._breed = None
-        self.name = name
+APPROVED_BREEDS = [
+    "Mastiff",
+    "Chihuahua",
+    "Corgi",
+    "Shar Pei",
+    "Beagle",
+    "French Bulldog",
+    "Pug",
+    "Pointer"
+]
+
+class Dog:
+    pass
+    def __init__(self, name="None", breed=""):
+        self._name = name
         self.breed = breed
 
-    @property
-    def name(self):
+        if not name:
+            print("Name must be string between 1 and 25 characters.")
+        elif not isinstance(name, str):
+            print("Name must be string between 1 and 25 characters.")
+        elif not 1 <= len(name) <= 25:
+            print("Name must be string between 1 and 25 characters.")
+        else:
+            self._name = name
+
+
+    def get_name(self):
         return self._name
 
-    @name.setter
-    def name(self, value):
-        if not isinstance(value, str):
-            print("Name must be a string.")
-        elif not (1 <= len(value) <= 25):
-            print("Name must be between 1 and 25 characters.")
+    def set_name(self, value):
+        if not value:
+            print("Name must be string between 1 and 25 characters.\n")
+        elif not isinstance(value, str) or not (1 <= len(value) <= 25):
+            print("Name must be string between 1 and 25 characters.\n")
         else:
             self._name = value
 
-    @property
-    def breed(self):
+    name = property(get_name, set_name)
+
+    def get_breed(self):
         return self._breed
 
-    @breed.setter
-    def breed(self, value):
-        if not isinstance(value, str):
-            print("Breed must be a string.")
-        elif value not in self.approved_breeds:
-            print("Breed must be in the list of approved breeds.")
+    def set_breed(self, value):
+        if value != "" and value not in APPROVED_BREEDS:
+            print("Breed must be in list of approved breeds.")
+            self._breed = None
         else:
             self._breed = value
+
+
+    breed = property(get_breed, set_breed)
--- lib/person.py
+++ lib/person.py
@@ -1,68 +1,63 @@
-import io
+#!/usr/bin/env python3
+
+APPROVED_JOBS = [
+    "Admin",
+    "Customer Service",
+    "Human Resources",
+    "ITC",
+    "Production",
+    "Legal",
+    "Finance",
+    "Sales",
+    "General Management",
+    "Research & Development",
+    "Marketing",
+    "Purchasing"
+]
 
 class Person:
-    APPROVED_JOBS = [
-        "Admin", "Customer Service", "Human Resources", "ITC", "Production",
-        "Legal", "Finance", "Sales", "General Management", "Research & Development",
-        "Marketing", "Purchasing"
-    ]
+    pass
+    def __init__(self, name="None", job=""):
+        self._name = name
+        self._job = job
 
-    def __init__(self, name="", job=""):
-        self._name = None
-        self._job = None
-        self.name = name
-        self.job = job
+        if not name:
+            print("Name must be string between 1 and 25 characters.")
+        elif not isinstance(name, str):
+            print("Name must be string between 1 and 25 characters.")
+        elif not 1 <= len(name) <= 25:
+            print("Name must be string between 1 and 25 characters.")
+        else:
+            self._name = name.title()
+        if job and job not in APPROVED_JOBS:
+            print("Job must be in list of approved jobs.")
+            self._job = None
+        else:
+            self._job = job
+        
 
-    @property
-    def name(self):
+    def get_name(self):
         return self._name
 
-    @name.setter
-    def name(self, value):
+    def set_name(self, value):
         if not isinstance(value, str) or not (1 <= len(value) <= 25):
-            print("Name must be a string under 25 characters.")
+            print("Name must be string between 1 and 25 characters.")
         else:
             self._name = value.title()
 
-    @property
-    def job(self):
+    name = property(get_name, set_name)
+
+    def get_job(self):
         return self._job
 
-    @job.setter
-    def job(self, value):
-        if value not in self.APPROVED_JOBS:
-            print("Job must be in the list of approved jobs.")
+    def set_job(self, value):
+        if value != "" and value not in APPROVED_JOBS:
+            print("Job must be in list of approved jobs.")
         else:
             self._job = value
 
 
-captured_out = io.StringIO()
-
-
-captured_out = io.StringIO()  
-person1 = Person(name="", job="Sales")
-print(captured_out.getvalue().strip())
-assert captured_out.getvalue().strip() == "Name must be a string under 25 characters."
 
 
 
-captured_out = io.StringIO()  
-person2 = Person(name=123, job='Sales')
-print(captured_out.getvalue().strip())
-assert captured_out.getvalue().strip() == "Name must be a string under 25 characters."
-
-
-captured_out = io.StringIO()  
-person3 = Person(name="What do Persons do on their day off? Can't lie around - that's their job.", job='Sales')
-print(captured_out.getvalue().strip())
-assert captured_out.getvalue().strip() == "Name must be a string under 25 characters."
-
-
-captured_out = io.StringIO()  
-person4 = Person(name="John Doe", job='Sales')
-
-
-captured_out = io.StringIO()  
-person5 = Person(job="Benevolent dictator for life")
-print(captured_out.getvalue().strip())
-assert captured_out.getvalue().strip() == "Job must be in the list of approved jobs."
+    job = property(get_job, set_job)
